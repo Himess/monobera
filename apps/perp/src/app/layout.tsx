a2868f2dc4d7979aa22a4c4204b117d5aa18a852
bearpong <178402093+bearpong@users.noreply.github.com>
2025-01-06 15:51:12 -0500

    chore: removes hotjar Refs BFE-473

--- apps/perp/src/app/layout.tsx
+++ apps/perp/src/app/layout.tsx
@@ -29,20 +29,6 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <Script
-        id="HotJarAnalytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:3728321,hjsv:6};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`,
-        }}
-      />
       <body
         className={cn("bg-background font-sans antialiased", fontSans.variable)}
       >
