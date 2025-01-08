272decfbb9984aff9867c60278529655dbeedf26
bearpong <178402093+bearpong@users.noreply.github.com>
2025-01-08 10:21:58 -0500

    chore(graphql): removes unused files

--- packages/graphql/src/modules/index.ts
+++ packages/graphql/src/modules/index.ts
@@ -3,7 +3,4 @@ export * from "./types";
 export * from "./chain/chain.codegen";
 export * from "./honey";
 export * from "./lend";
-// export * from "./governance";
-export * from "./berachef";
 export * from "./clients";
-// export * from "./pol";
