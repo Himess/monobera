99f311c4caf383caaabe32b72364068036be15ad
bearpong <178402093+bearpong@users.noreply.github.com>
2025-01-10 12:44:10 -0500

    test: minor examples

--- packages/berajs/src/actions/dex/getOnChainPool.test.ts
+++ packages/berajs/src/actions/dex/getOnChainPool.test.ts
@@ -6,7 +6,8 @@ describe("getOnChainPool", () => {
   it("should be defined", () => {
     expect(process.env.NEXT_PUBLIC_POL_SUBGRAPH_URL).toBeTruthy();
   });
-  it("should return the on chain pool", async () => {
+
+  it("should fail on invalid pool id", async () => {
     const invalidPoolPromise = getOnChainPool({
       poolId:
         "0x4e7ab786f93737026711ab3165aac911746b6613000000000000000000000011",
--- packages/berajs/src/actions/pol/getAllValidators.test.ts
+++ packages/berajs/src/actions/pol/getAllValidators.test.ts
@@ -1,21 +1,36 @@
 import { describe, it, expect } from "vitest";
-// import { getAllValidators } from "./get-all-validators";
+import { getAllValidators } from "./get-all-validators";
 
-// import { GetValidators } from "@bera/graphql/pol/api";
+import { GetValidators } from "@bera/graphql/pol/api";
 
-// describe("getAllValidators", () => {
-//   it("should be defined", () => {
-//     expect(GetValidators).toBeDefined();
-//     expect(process.env.NEXT_PUBLIC_POL_SUBGRAPH_URL).toBeTruthy();
-//   });
-// });
+describe("getAllValidators", () => {
+  it("should be defined", () => {
+    expect(GetValidators).toBeDefined();
+    expect(process.env.NEXT_PUBLIC_POL_SUBGRAPH_URL).toBeTruthy();
+  });
+});
 
-// describe("getAllValidators", () => {
-//   it("should return all validators", async () => {
-//     const validators = await getAllValidators();
-//     expect(validators?.validators?.validators.length).toBeGreaterThan(0);
-//   });
-// });
+describe("getAllValidators", async () => {
+  const result = await getAllValidators();
+
+  if (!result) {
+    throw new Error("Failed to fetch validators");
+  }
+
+  const { validators } = result;
+  it("should return all validators", async () => {
+    expect(validators?.validators?.length).toBeGreaterThan(0);
+  });
+
+  it("should have consistent data", async () => {
+    const totalBgtCatpure = validators?.validators.reduce(
+      (acc, validator) =>
+        acc + Number(validator.dynamicData?.bgtCapturePercentage ?? 0),
+      0,
+    );
+    expect(totalBgtCatpure).toBe(1);
+  });
+});
 
 describe("getAllValidators", () => {
   it("should be defined", () => {
